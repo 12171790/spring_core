588f21a63d79d1fd7af2401519ddb040a4d1e759
Bae Ki Woong <12171790@inha.edu>
2025-08-25 22:36:57 +0900

    할인 정책과 테스트 코드

---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,40 @@
+package hello.core.order;
+
+public class Order {
+    private Long MemberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public Order(Long MemberId, String itemName, int itemPrice, int discountPrice) {
+        this.MemberId = MemberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    public int calculateDiscount() {
+        return itemPrice - discountPrice;
+    }
+
+    public Long getMemberId() {
+        return MemberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [MemberId=" + MemberId + ", itemName=" + itemName + ", itemPrice=" + itemPrice + ", discountPrice=" + discountPrice + "]";
+    }
+}
